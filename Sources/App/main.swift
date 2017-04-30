4a5655779ca9ff585038e26b04e913b9dd4bb62f
João Carreira <joaocarreira@me.com>
2017-04-30 21:23:41 +0200

    Hello world with Vapor

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,12 +2,43 @@ import Vapor
 
 let drop = Droplet()
 
+// GET requests
+
 drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+
+    // returning a String
+    //    return "Hello Vapor"
+    
+    // returning JSON
+    return try JSON(node: ["message": "Hello Vapor"])
 }
 
-drop.resource("posts", PostController())
+drop.get("hello") { req in
+
+    return try JSON(node: ["message": "Hello page"])
+}
+
+drop.get("hello", "there") { req in
+    
+    return try JSON(node: ["message": "Hello there page"])
+}
+
+drop.get("hello", Int.self) { req, number in
+ 
+    return try JSON(node: ["message": "You passed numer \(number)"])
+}
+
+
+// POST requests
+
+drop.post("post") { req in
+    
+    guard let name = req.data["name"]?.string else {
+        throw Abort.badRequest
+    }
+    
+    return try JSON(node: ["message": "Hello \(name)"])
+}
+
 
 drop.run()
