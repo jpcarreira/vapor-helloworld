aa79c6d609fb3c4ff9d538f6ac21a1c07fa2f75c
João Carreira <joaocarreira@me.com>
2017-05-01 00:17:03 +0200

    templating with leaf

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -28,6 +28,43 @@ drop.get("hello", Int.self) { req, number in
     return try JSON(node: ["message": "You passed numer \(number)"])
 }
 
+// using leaf templates
+
+drop.get("template1") { req in
+ 
+    return try drop.view.make("hello", Node(node: ["name": "John Doe"]))
+}
+
+drop.get("template2", String.self) { req, name in
+    
+    return try drop.view.make("hello", Node(node: ["name": name]))
+}
+
+drop.get("template3") { req in
+    
+    let users = try ["John Doe", "Jane Doe", "John Doe", "Jane Doe", "John Doe", "Jane Doe"].makeNode()
+    return try drop.view.make("hello2", Node(node: ["users": users]))
+}
+
+drop.get("template4") { req in
+    
+    let users = try [
+        ["name": "John Doe", "email": "johndoe@mail.com"].makeNode(),
+        ["name": "John Doe", "email": "johndoe@mail.com"].makeNode(),
+        ["name": "John Doe", "email": "johndoe@mail.com"].makeNode()
+        ].makeNode()
+    
+    return try drop.view.make("hello3", Node(node: ["users": users]))
+}
+
+drop.get("template5") { req in
+    
+    guard let sayHello = req.data["sayHello"]?.bool else {
+        throw Abort.badRequest
+    }
+    
+    return try drop.view.make("hello4", Node(node: ["sayHello": sayHello]))
+}
 
 // POST requests
 
